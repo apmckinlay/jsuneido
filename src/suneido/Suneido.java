dd7ded84b8a4a2a21c722f276d0b763a93c23524
Andrew McKinlay <apmckinlay@gmail.com>
2008-05-14 21:24:07 +0000

    cleaned up

--- src/suneido/Suneido.java
+++ src/suneido/Suneido.java
@@ -3,17 +3,5 @@ package suneido;
 public class Suneido {
 	public static void main(String args[]) {
 		System.out.println("hello world");
-		
-		SuValue vs = new SuString("ok");
-		System.out.println("vs " + vs);
-		
-		SuValue vi = new SuInteger(1);
-		System.out.println("vi " + vi);
-		
-		SuContainer c = new SuContainer();
-		c.append(new SuString("zero"));
-		c.putdata(new SuString("name"), new SuString("andrew"));
-		c.putdata(vi, vs);
-		System.out.println("c " + c);
 	}
 }
