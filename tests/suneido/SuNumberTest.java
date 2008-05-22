5fac13524334d052d1cb06cfc6dc3e2e334b5bce
Andrew McKinlay <apmckinlay@gmail.com>
2008-05-22 18:48:57 +0000

    got number pack/unpack working

---
+++ tests/suneido/SuNumberTest.java
@@ -0,0 +1,23 @@
+package suneido;
+
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SuNumberTest {
+	@Test
+	public void pack() {
+		String[] values = { 
+				"1", "10", "123", "1000", "9999", "10000", "10002", "100020000", "100020003","1000200030004",
+				".12", ".1", ".01", ".001", ".0001", ".00010002", ".00001"
+				};
+		for (String s : values) {
+			SuValue x = new SuDecimal(s);
+			ByteBuffer buf = ByteBuffer.allocate(x.packsize());
+			x.pack(buf);
+			SuValue y = SuValue.unpack(buf);
+			assertEquals(x, y);
+		}
+	}
+}
