dd041088f082392c224658e20c282ca106c08d50
amckinlay <amckinlay@localhost>
2008-06-19 17:33:36 +0000

    using Channel and Selector but single threaded

--- src/suneido/Server.java
+++ src/suneido/Server.java
@@ -1,64 +1,91 @@
 package suneido;
 
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
 import java.io.*;
 
+/**
+ * See:<ul>
+ * <li>TCP/IP Sockets in Java 2nd Ed.
+ * <li><a href="http://javanio.info/filearea/nioserver/NIOServerMark2.pdf">
+ *		How to Build a Scalable Multiplexed Server with NIO Mark II</a>
+ * <li><a href="http://today.java.net/cs/user/print/a/350">
+ * 		Architecture of a Highly Scalable NIO-Based Server</a>
+ * </ul>
+ * <b>NOTE: This version is single threaded.</b>
+ * @author Andrew McKinlay
+ */
 public class Server {
 	static final int PORT = 3456;
-	static final int NTHREADS = 1;
 	
-	public static void main(String[] args) {
-		start();
+	public static void main(String[] args) throws IOException {
+		run(PORT);
 	}
 	
-	public static void start() {
-		final ServerSocket serverSocket = open();
-		for (int i = 1; i < NTHREADS; ++i) {
-			Thread thread = new Thread() {
-				public void run() {
-					dispatch(serverSocket);
-				}
-			};
-		thread.start();
+	static Selector selector;
+	
+	public static void run(int port) throws IOException {
+		selector = Selector.open();
+		ServerSocketChannel listenChannel = ServerSocketChannel.open();
+		// to avoid problems restarting server
+		// as recommended by Effective TCP/IP Programming
+		listenChannel.socket().setReuseAddress(true);
+		listenChannel.socket().bind(new InetSocketAddress(port));
+		listenChannel.configureBlocking(false);
+		listenChannel.register(selector, SelectionKey.OP_ACCEPT);		
+		while (true) {
+			selector.select();
+			Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+			while (keyIter.hasNext()) {
+				SelectionKey key = keyIter.next();
+				if (key.isAcceptable())
+					handleAccept(key);
+				else if (key.isReadable())
+					handleRead(key);
+				else if (key.isWritable())
+					handleWrite(key);
+				keyIter.remove();
+			}
 		}
-		dispatch(serverSocket);
+	}
+	
+	public static void handleAccept(SelectionKey key) throws IOException {
+		SocketChannel channel = ((ServerSocketChannel) key.channel()).accept();
+		channel.configureBlocking(false);
+		channel.register(key.selector(), SelectionKey.OP_READ, ByteBuffer.allocate(1000));
 	}
 
-	private static ServerSocket open() {
-		try {
-			return new ServerSocket(PORT);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-			throw new SuException("can't open port " + PORT);
+	public static void handleRead(SelectionKey key) throws IOException {
+		SocketChannel channel = (SocketChannel) key.channel();
+		ByteBuffer buf = (ByteBuffer) key.attachment();
+		int bytesRead = channel.read(buf);
+		if (bytesRead == -1)
+			channel.close();
+		else if (bytesRead > 0 && requestComplete(buf)) {
+			handleRequest(key, buf);
 		}
 	}
 	
-	private static void dispatch(final ServerSocket serverSocket) {
-		Socket socket;
-		while (true) {
-			try {
-				socket = serverSocket.accept();
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-				continue ;
-			}
-			try {
-				InputStream in = socket.getInputStream();
-				OutputStream out = socket.getOutputStream();
-				out.write("hello world\n".getBytes(), 0, 12);
-				byte[] buf = new byte[100];
-				int n;
-				while (-1 != (n = in.read(buf)))
-					out.write(buf, 0, n);
-			} catch (IOException e) {
-				System.out.println(e.getMessage());
-			} finally {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					// ignore
-				}
-			}
-		}
+	public static void handleRequest(SelectionKey key, ByteBuffer buf) throws IOException {
+		//TODO just echo for now
+		buf.flip();
+		key.interestOps(SelectionKey.OP_WRITE);
 	}
+
+	private static boolean requestComplete(ByteBuffer buf) {
+		return true; //TODO check for newline
+	}
+
+	private static void handleWrite(SelectionKey key) throws IOException {
+		ByteBuffer buf = (ByteBuffer) key.attachment();
+		SocketChannel channel = (SocketChannel) key.channel();
+		channel.write(buf);
+		if (buf.hasRemaining())
+			return ;
+		buf.clear();
+		key.interestOps(SelectionKey.OP_READ);
+	}
+
 }
