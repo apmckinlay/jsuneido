308779488dfaf1f2dea0251a0481e372afdd9d8e
Andrew McKinlay <apmckinlay@gmail.com>
2009-10-14 22:07:52 +0000

    added PersistentList, improved PersistentMap

---
+++ src/suneido/util/PersistentList.java
@@ -0,0 +1,240 @@
+package suneido.util;
+
+import java.util.*;
+
+import com.google.common.base.Joiner;
+
+/**
+ * A persistent immutable single linked list class.
+ *
+ * @author Andrew McKinlay
+ */
+public class PersistentList<T> extends AbstractSequentialList<T> {
+
+	private final T value;
+	// logically final, but Builder uses mutable private copies
+	private PersistentList<T> next;
+
+	private PersistentList(T value, PersistentList<T> next) {
+		this.value = value;
+		this.next = next;
+	}
+
+	@SuppressWarnings("unchecked")
+	private final static PersistentList Nil = makeNil();
+
+	@SuppressWarnings("unchecked")
+	private static PersistentList makeNil() {
+		PersistentList nil = new PersistentList(null, null);
+		nil.next = nil;
+		return nil;
+	}
+
+	/** @return The empty list */
+	@SuppressWarnings("unchecked")
+	public static <T> PersistentList<T> nil() {
+		return Nil;
+	}
+
+	/** @return A list containing the specified value */
+	@SuppressWarnings("unchecked")
+	public static <T> PersistentList<T> of(T value) {
+		return new PersistentList<T>(value, Nil);
+	}
+
+	/** @return A list containing the specified values */
+	@SuppressWarnings("unchecked")
+	public static <T> PersistentList<T> of(T v1, T v2) {
+		return Nil.with(v2).with(v1);
+	}
+
+	/** @return A list containing the specified values */
+	@SuppressWarnings("unchecked")
+	public static <T> PersistentList<T> of(T v1, T v2, T v3) {
+		return Nil.with(v3).with(v2).with(v1);
+	}
+
+	/** @return A list containing the specified values */
+	@SuppressWarnings("unchecked")
+	public static <T> PersistentList<T> of(T... values) {
+		PersistentList<T> list = Nil;
+		for (int i = values.length - 1; i >= 0; --i)
+			list = list.with(values[i]);
+		return list;
+	}
+
+	/** @return A new list with the values from the Iterable */
+	public static <T> PersistentList<T> copyOf(Iterable<T> values) {
+		return new Builder<T>().addAll(values).build();
+	}
+
+	/** @return The value at the start of the list */
+	public T head() {
+		return value;
+	}
+
+	/** @return The remainder of the list without the first element */
+	public PersistentList<T> tail() {
+		return next;
+	}
+
+	/** @return A new list with value as the head and the old list as the tail */
+	public PersistentList<T> with(T value) {
+		assert value != null;
+		return new PersistentList<T>(value, this);
+	}
+
+	@Override
+	public T get(int i) {
+		if (i >= 0)
+			for (PersistentList<T> list = this; list != Nil; list = list.tail())
+				if (i-- == 0)
+					return list.head();
+		throw new IndexOutOfBoundsException();
+	}
+
+	/**
+	 * Note: O(N)
+	 * @return A new list omitting the specified value
+	 */
+	public PersistentList<T> without(T x) {
+		if (!contains(x))
+			return this;
+		Builder<T> save = new Builder<T>();
+		PersistentList<T> list = this;
+		do {
+			for (; list != Nil && !x.equals(list.head()); list = list.tail())
+				save.add(list.head());
+			while (list != Nil && x.equals(list.head()))
+				list = list.tail();
+		} while (list.contains(x));
+		// list is now the longest tail that doesn't contain x
+		return save.buildOnto(list);
+	}
+
+	/** Note: O(N) */
+	@Override
+	public boolean contains(Object value) {
+		for (PersistentList<T> list = this; list != Nil; list = list.next)
+			if (value.equals(list.head()))
+				return true;
+		return false;
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public boolean equals(Object other) {
+		if (this == other)
+			return true;
+		if (!(other instanceof PersistentList))
+			return false;
+		PersistentList<? extends Object> x = this;
+		PersistentList<Object> y = (PersistentList<Object>) other;
+		for (; x != Nil && y != Nil; x = x.tail(), y = y.tail())
+			if (!x.head().equals(y.head()))
+				return false;
+		return x == Nil && y == Nil;
+	}
+
+	public static final Joiner commaJoiner = Joiner.on(",");
+
+	@Override
+	public String toString() {
+		return "(" + commaJoiner.join(this) + ")";
+	}
+
+	/**
+	 * Note: O(N)
+	 * @return The number of elements in the list
+	 */
+	@Override
+	public int size() {
+		int size = 0;
+		for (PersistentList<T> list = this; list != Nil; list = list.next)
+			++size;
+		return size;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this == Nil;
+	}
+
+	/** @return A new list with the elements in the reverse order */
+	public PersistentList<T> reversed() {
+		PersistentList<T> list = nil();
+		for (PersistentList<T> p = this; p != Nil; p = p.next)
+			list = list.with(p.value);
+		return list;
+	}
+
+	public static class Builder<T> {
+
+		private PersistentList<T> list = nil();
+
+		public Builder<T> add(T value) {
+			list = list.with(value);
+			return this;
+		}
+
+		public Builder<T> addAll(Iterable<? extends T> values) {
+			for (T value : values)
+				add(value);
+			return this;
+		}
+
+		/**
+		 * The Builder cannot be used after calling build()
+		 * @return The list
+		 */
+		@SuppressWarnings("unchecked")
+		public PersistentList<T> build() {
+			return buildOnto((PersistentList<T>) nil());
+		}
+
+		public PersistentList<T> buildOnto(PersistentList<T> tail) {
+			// reverse in place by changing pointers (no allocation)
+			for (PersistentList<T> p = list; p != nil();) {
+				PersistentList<T> next = p.next;
+				p.next = tail;
+				tail = p;
+				p = next;
+			}
+			list = null;
+			return tail;
+		}
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		return new Iter<T>(this);
+	}
+
+	private static class Iter<T> implements Iterator<T> {
+
+		private PersistentList<T> list;
+
+		private Iter(PersistentList<T> list) {
+			this.list = new PersistentList<T>(null, list);
+		}
+
+		public boolean hasNext() {
+			return list.tail() != Nil;
+		}
+
+		public T next() {
+			list = list.tail();
+			return list.head();
+		}
+
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int index) {
+		throw new UnsupportedOperationException();
+	}
+
+}
--- src/suneido/util/PersistentMap.java
+++ src/suneido/util/PersistentMap.java
@@ -5,42 +5,35 @@ import java.util.*;
 import net.jcip.annotations.Immutable;
 
 /**
- * An implementation of an immutable persistent map. Based on Phil Bagwell's
- * Hash Array Mapped Trie with some help from Rich Hickey's implementation in
- * Clojure. Uses OverflowNodes instead of extended hashing. The primary goal was
- * simplicity, not speed or space performance.
+ * An persistent immutable map. Based on Phil Bagwell's Hash Array Mapped Trie
+ * with some help from Rich Hickey's implementation in Clojure. Uses
+ * OverflowNodes instead of extended hashing and does not resize root.
  *
  * @author Andrew McKinlay
  */
 @Immutable
-public class PersistentMap<K, V> extends AbstractMap<K, V> {
-
-	private final int size;
-	private final Node<K, V> node;
+public abstract class PersistentMap<K, V> extends AbstractMap<K, V> {
 
 	private static final int BITS_PER_LEVEL = 5;
 	private static final int LEVEL_MASK = (1 << BITS_PER_LEVEL) - 1;
 	private static final int HASH_BITS = 32;
 
 	@SuppressWarnings("unchecked")
-	public static final <K, V> PersistentMap<K, V> empty() {
-		return EmptyMap.emptyMap;
-	}
+	private static TrieNode emptyNode = new TrieNode(0, new Entry[0]);
 
-	/** lazy initialization */
 	@SuppressWarnings("unchecked")
-	private static class EmptyMap {
-		static PersistentMap emptyMap = new PersistentMap(0, emptyNode);
+	public static final <K, V> PersistentMap<K, V> empty() {
+		return emptyNode;
 	}
 
-	private PersistentMap(int size, Node<K, V> node) {
-		this.size = size;
-		this.node = node;
-	}
+	public abstract PersistentMap<K, V> with(K key, V value);
+
+	public abstract PersistentMap<K, V> without(K key);
 
 	@Override
 	public int size() {
-		return size;
+		// TODO size
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -49,70 +42,64 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 	}
 
 	@Override
-	public V get(Object key) {
-		return node.get(key, key.hashCode(), 0);
-	}
-
-	// PERF would be nice to eliminate the Added allocation,
-	// maybe by creating and using the PersistentMap object
-	// since it will normally be created anyway
-	// although that would require making size not final
-	public PersistentMap<K, V> with(K key, V value) {
-		assert key != null;
-		assert value != null;
-		Added added = new Added();
-		Node<K, V> n = node.with(key, value, key.hashCode(), 0, added);
-		return n == node ? this // map already had it
-				: new PersistentMap<K, V>(size + added.n, n);
-	}
-
-	@SuppressWarnings("unchecked")
-	public PersistentMap<K, V> without(Object key) {
-		Node<K, V> n = (Node<K, V>) node.without(key, key.hashCode(), 0);
-		return n == node ? this // map didn't have it
-				: n == null ? EmptyMap.emptyMap
-				: new PersistentMap<K, V>(size - 1, n);
-	}
-
-	@Override
 	public V remove(Object key) {
 		throw new UnsupportedOperationException();
 	}
 
-	private static class Added {
-		public int n = 0;
-	}
+	private static abstract class Node<K, V> extends PersistentMap<K, V> {
+		@Override
+		public V get(Object key) {
+			return get(key, key.hashCode(), 0);
+		}
 
-	@SuppressWarnings("unchecked")
-	private static TrieNode emptyNode = new TrieNode(0, new Entry[0]);
+		@Override
+		public PersistentMap<K, V> with(K key, V value) {
+			assert key != null;
+			assert value != null;
+			return with(key, value, key.hashCode(), 0);
+		}
 
-	private static abstract class Node<K, V> {
-		public abstract V get(Object key, int hash, int shift);
+		@Override
+		@SuppressWarnings("unchecked")
+		public PersistentMap<K, V> without(Object key) {
+			Node<K, V> n = (Node<K, V>) without(key, key.hashCode(), 0);
+			return n == null ? emptyNode : n;
+		}
 
-		public abstract Node<K, V> with(K key, V value, int hash, int shift,
-				Added added);
+		protected abstract Node<K, V> with(K key, V value, int hash, int shift);
 
 		/**
 		 * @return either a Node, or null if empty, or sole remaining
 		 *         SimpleImmutableEntry from OverflowNode
 		 */
-		public abstract Object without(Object key, int hash, int shift);
+		protected abstract Object without(Object key, int hash, int shift);
+
+		protected abstract V get(Object key, int hash, int shift);
+
+		// Only used by Builder
+		protected void add(K key, V value) {
+			add(key, value, key.hashCode(), 0);
+		}
+
+		// Only used by Builder
+		protected abstract void add(K key, V value, int hash, int shift);
 	}
 
 	@Immutable
 	@SuppressWarnings("unchecked")
 	private static class TrieNode<K, V> extends Node<K, V> {
 
-		final int bitmap; // 1's indicate existing slots in a
-		final Object a[]; // size = number of 1's in bitmap
+		// normally final/immutable but modified by Builder via add on private copy
+		int bitmap; // 1's indicate existing slots in a
+		Object a[]; // size = number of 1's in bitmap
 
-		TrieNode(int bitmap, Object a[]) {
+		protected TrieNode(int bitmap, Object a[]) {
 			this.bitmap = bitmap;
 			this.a = a;
 		}
 
 		@Override
-		public V get(Object key, int hash, int shift) {
+		protected V get(Object key, int hash, int shift) {
 			int bit = bit(hash, shift);
 			if ((bitmap & bit) == 0)
 				return null; // slot empty
@@ -128,7 +115,41 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 		}
 
 		@Override
-		public TrieNode<K, V> with(K key, V value, int hash, int shift, Added added) {
+		protected void add(K key, V value, int hash, int shift) {
+			SimpleImmutableEntry<K, V> assoc;
+			int bit = bit(hash, shift);
+			int i = Integer.bitCount(bitmap & (bit - 1));
+			if ((bitmap & bit) == 0) { // not found
+				int len = length();
+				if (len + 1 > a.length)
+					a = Arrays.copyOf(a, a.length * 2);
+				bitmap |= bit;
+				System.arraycopy(a, i, a, i + 1, len - i);
+				// fall thru to bottom
+			} else if (a[i] instanceof SimpleImmutableEntry) {
+				assoc = (SimpleImmutableEntry<K, V>) a[i];
+				if (assoc.getKey().equals(key)) {
+					if (assoc.getValue().equals(value))
+						return ; // entry already exists
+					// fall through to bottom
+				} else { // collision
+					// push entry into child node along with new entry
+					a[i] = newChild(assoc, key, value, hash,
+							shift + BITS_PER_LEVEL);
+					return ;
+				}
+			} else { // slot points to child node
+				Node<K, V> child = (Node<K, V>) a[i];
+				Node<K, V> newchild = child.with(key, value, hash,
+						shift + BITS_PER_LEVEL);
+				a[i] = newchild;
+				return;
+			}
+			a[i] = new SimpleImmutableEntry<K, V>(key, value);
+		}
+
+		@Override
+		protected TrieNode<K, V> with(K key, V value, int hash, int shift) {
 			SimpleImmutableEntry<K, V> assoc;
 			int bm;
 			Object aa[];
@@ -136,7 +157,6 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 			int i = Integer.bitCount(bitmap & (bit - 1));
 			if ((bitmap & bit) == 0) { // not found
 				aa = new Object[a.length + 1];
-				added.n = 1;
 				bm = bitmap | bit;
 				System.arraycopy(a, 0, aa, 0, i);
 				System.arraycopy(a, i, aa, i + 1, a.length - i);
@@ -151,7 +171,6 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 					// fall through to bottom
 				} else { // collision
 					// push entry into child node along with new entry
-					added.n = 1;
 					aa = a.clone();
 					aa[i] =
 							newChild(assoc, key, value, hash, shift
@@ -161,8 +180,7 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 			} else { // slot points to child node
 				Node<K, V> child = (Node<K, V>) a[i];
 				Node<K, V> newchild =
-						child.with(key, value, hash, shift + BITS_PER_LEVEL,
-								added);
+						child.with(key, value, hash, shift + BITS_PER_LEVEL);
 				if (newchild == child)
 					return this; // entry already exists
 				aa = a.clone();
@@ -183,9 +201,7 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 			int h = (hash >>> shift) & LEVEL_MASK;
 			if (ha == h) { // collision
 				Object[] aa = new Object[1];
-				aa[0] =
-						newChild(assoc, key, value, hash, shift
-								+ BITS_PER_LEVEL);
+				aa[0] = newChild(assoc, key, value, hash, shift	+ BITS_PER_LEVEL);
 				return new TrieNode<K, V>(1, aa);
 			}
 			Object[] aa = new Object[2];
@@ -203,7 +219,7 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 		}
 
 		@Override
-		public Object without(Object key, int hash, int shift) {
+		protected Object without(Object key, int hash, int shift) {
 			int bit = bit(hash, shift);
 			if ((bitmap & bit) == 0)
 				return this; // slot empty
@@ -235,15 +251,24 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 			return bit;
 		}
 
+		private int length() {
+			// could use bitCount instead
+			int i = a.length - 1;
+			while (i > 0 && a[i] == null)
+				--i;
+			return i + 1;
+		}
+
 	}
 
-	/** used for overflow leaf nodes when multiple identical hash */
+	/** Used for overflow leaf nodes when multiple identical hash */
 	@Immutable
 	@SuppressWarnings("unchecked")
 	private static class OverflowNode<K, V> extends Node<K, V> {
-		private final SimpleImmutableEntry<K, V> assocs[];
+		// normally final but modified by Builder via add on private copy
+		private SimpleImmutableEntry<K, V> assocs[];
 
-		public OverflowNode(SimpleImmutableEntry<K, V> assoc1,
+		private OverflowNode(SimpleImmutableEntry<K, V> assoc1,
 				SimpleImmutableEntry<K, V> assoc2) {
 			this.assocs = new SimpleImmutableEntry[2];
 			assocs[0] = assoc1;
@@ -255,54 +280,98 @@ public class PersistentMap<K, V> extends AbstractMap<K, V> {
 		}
 
 		@Override
-		public V get(Object key, int hash, int shift) {
+		protected V get(Object key, int hash, int shift) {
 			int i = find(key);
 			return i == -1 ? null : assocs[i].getValue();
 		}
 
 		@Override
-		public OverflowNode<K, V> with(K key, V value, int hash, int shift,
-				Added added) {
+		protected void add(K key, V value, int hash, int shift) {
+			int i = find(key);
+			if (i == -1) { // key not found
+				i = length();
+				if (i > assocs.length - 1)
+					assocs = Arrays.copyOf(assocs, assocs.length * 2);
+			}
+			assocs[i] = new SimpleImmutableEntry<K, V>(key, value);
+		}
+
+		@Override
+		protected OverflowNode<K, V> with(K key, V value, int hash, int shift) {
 			int i = find(key);
 			SimpleImmutableEntry<K, V>[] a;
 			if (i == -1) { // key not found
-				a = Arrays.copyOf(assocs, assocs.length + 1);
-				i = assocs.length;
-				added.n = 1;
+				i = length();
+				a = Arrays.copyOf(assocs, i + 1);
 			} else if (assocs[i].getValue().equals(value))
 				return this; // already there
 			else
-				a = assocs.clone();
+				// key exists but value wrong
+				a = Arrays.copyOf(assocs, length());
 			a[i] = new SimpleImmutableEntry<K, V>(key, value);
 			return new OverflowNode<K, V>(a);
 		}
 
 		@Override
-		public Object without(Object key, int hash, int shift) {
+		protected Object without(Object key, int hash, int shift) {
 			int i = find(key);
 			if (i == -1)
 				return this; // not there
-			if (assocs.length == 2)
+			if (length() == 2)
 				return assocs[i ^ 1];
-			SimpleImmutableEntry<K, V> a[] =
-					new SimpleImmutableEntry[assocs.length - 1];
+			SimpleImmutableEntry<K, V> a[] = new SimpleImmutableEntry[length() - 1];
 			System.arraycopy(assocs, 0, a, 0, i);
-			System.arraycopy(assocs, i + 1, a, i, assocs.length - i - 1);
+			System.arraycopy(assocs, i + 1, a, i, a.length - i);
 			return new OverflowNode<K, V>(a);
 		}
 
 		private int find(Object key) {
-			for (int i = 0; i < assocs.length; ++i)
+			for (int i = 0; i < length(); ++i)
 				if (assocs[i].getKey().equals(key))
 					return i;
 			return -1;
+		}
+
+		private int length() {
+			int i = assocs.length - 1;
+			while (i > 0 && assocs[i] == null)
+				--i;
+			return i + 1;
 		}
 
 	}
 
 	@Override
 	public Set<Map.Entry<K, V>> entrySet() {
+		// TODO entrySet
 		throw new UnsupportedOperationException();
+	}
+
+	public static class Builder<K, V> {
+
+		private Node<K, V> map = new TrieNode<K, V>(0, new Entry[4]);
+
+		public Builder<K, V> put(K key, V value) {
+			map.add(key, value);
+			return this;
+		}
+
+		public Builder<K, V> putAll(Map<? extends K, ? extends V> m) {
+			for (Entry<? extends K, ? extends V> e : m.entrySet())
+				map.add(e.getKey(), e.getValue());
+			return this;
+		}
+
+		/**
+		 * The Builder cannot be used after calling build()
+		 * @return The working map, which may contain some unused space, but
+		 *         doesn't require copying.
+		 */
+		public PersistentMap<K, V> build() {
+			PersistentMap<K, V> result = map;
+			map = null;
+			return result;
+		}
 	}
 
 }
