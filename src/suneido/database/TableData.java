e62ad03bb12ef9facae0242d0f0f7db3551bedec
Andrew McKinlay <apmckinlay@gmail.com>
2009-10-15 20:41:34 +0000

    working on concurrency

--- src/suneido/database/TableData.java
+++ src/suneido/database/TableData.java
@@ -2,18 +2,26 @@ package suneido.database;
 
 import static suneido.Suneido.verify;
 
+// almost immutable but update will change record
 public class TableData {
 
 	private final Record record;
-	public int nextfield;
-	public int nrecords;
-	public int totalsize;
+	public final int nextfield;
+	public final int nrecords;
+	public final int totalsize;
 
 	public TableData(Record record) {
 		this.record = record;
 		nextfield = record.getInt(Table.NEXTFIELD);
 		nrecords = record.getInt(Table.NROWS);
 		totalsize = record.getInt(Table.TOTALSIZE);
+	}
+
+	private TableData(Record record, int nextfield, int nrecords, int totalsize) {
+		this.record = record;
+		this.nextfield = nextfield;
+		this.nrecords = nrecords;
+		this.totalsize = totalsize;
 	}
 
 	public int nrecords() {
@@ -28,27 +36,30 @@ public class TableData {
 		return nrecords == 0;
 	}
 
-	public void add(int recSize) {
-		++nrecords;
-		totalsize += recSize;
-		update();
+	public TableData with(int recSize) {
+		return new TableData(record, nextfield, nrecords + 1,
+				totalsize + recSize).update();
 	}
 
-	public void remove(int recSize) {
-		--nrecords;
-		totalsize -= recSize;
-		update();
+	public TableData without(int recSize) {
+		return new TableData(record, nextfield, nrecords - 1,
+				totalsize - recSize).update();
 	}
 
-	public void update(int oldSize, int newSize) {
-		totalsize += newSize - oldSize;
-		update();
+	public TableData withReplace(int oldSize, int newSize) {
+		return new TableData(record, nextfield, nrecords,
+				totalsize + newSize - oldSize).update();
 	}
 
-	public void update() {
+	public TableData withField() {
+		return new TableData(record, nextfield + 1, nrecords, totalsize).update();
+	}
+
+	public TableData update() {
 		verify(record.off() != 0);
 		record.truncate(Table.NEXTFIELD);
 		record.add(nextfield).add(nrecords).add(totalsize);
+		return this;
 	}
 
 }
