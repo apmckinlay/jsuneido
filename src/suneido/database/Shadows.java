1aa2760d735f608b0ef3efb6cf07642a818b440a
Andrew McKinlay <apmckinlay@gmail.com>
2009-11-19 20:39:54 +0000

    added read cache

--- src/suneido/database/Shadows.java
+++ src/suneido/database/Shadows.java
@@ -5,10 +5,12 @@ import java.util.*;
 import javax.annotation.concurrent.ThreadSafe;
 
 import suneido.util.ByteBuf;
+import suneido.util.LruCache;
 
 @ThreadSafe
 public class Shadows {
 	private final Map<Long, ByteBuf> shadows = new HashMap<Long, ByteBuf>();
+	private final LruCache<Long, ByteBuf> readcache = new LruCache<Long, ByteBuf>(20);
 
 	// used by complete > writeBtreeNodes
 	public synchronized Set<Map.Entry<Long, ByteBuf>> entrySet() {
@@ -18,15 +20,24 @@ public class Shadows {
 
 	public synchronized ByteBuf node(Destination dest, long offset) {
 		ByteBuf buf = shadows.get(offset);
-		if (buf == null)
-			buf = dest.adr(offset).readOnlyCopy(Btree.NODESIZE);
+		if (buf == null) {
+			buf = readcache.get(offset);
+			if (buf == null) {
+				buf = dest.adr(offset).copy(Btree.NODESIZE);
+				readcache.put(offset, buf);
+			}
+		}
 		return buf;
 	}
 
 	public synchronized ByteBuf nodeForWrite(Destination dest, long offset) {
 		ByteBuf buf = shadows.get(offset);
 		if (buf == null) {
-			buf = dest.adr(offset).copy(Btree.NODESIZE);
+			buf = readcache.get(offset);
+			if (buf == null)
+				buf = dest.adr(offset).copy(Btree.NODESIZE);
+			else
+				readcache.remove(offset);
 			shadows.put(offset, buf);
 			return buf;
 		} else if (!buf.isReadOnly())
@@ -37,6 +48,7 @@ public class Shadows {
 
 	public synchronized ByteBuf shadow(Destination dest, Long offset, ByteBuf copy) {
 		assert Thread.holdsLock(Transaction.commitLock);
+		readcache.remove(offset);
 		ByteBuf b = shadows.get(offset);
 		if (b == null) {
 			if (copy == null)
