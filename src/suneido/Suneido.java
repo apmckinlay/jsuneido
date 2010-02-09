233d0b0bfabd1037309a5eb6b3d9524bed569db6
amckinlay <amckinlay@localhost>
2010-02-09 22:34:02 +0000

    added command line options

--- src/suneido/Suneido.java
+++ src/suneido/Suneido.java
@@ -1,9 +1,31 @@
 package suneido;
 
+import suneido.database.Load;
+import suneido.database.server.DbmsServer;
+
 /**
  * @author Andrew McKinlay
- * <p><small>Copyright 2008 Suneido Software Corp. All rights reserved. 
+ * <p><small>Copyright 2008 Suneido Software Corp. All rights reserved.
  * Licensed under GPLv2.</small></p>
  */
 public class Suneido {
+	public static void main(String[] args) throws Exception {
+		CommandLineOptions options = new CommandLineOptions(args);
+		switch (options.action) {
+		case REPL:
+			Repl.main(null);
+			break;
+		case SERVER:
+			DbmsServer.main(null);
+			break;
+		case DUMP:
+			break;
+		case LOAD:
+			Load.main(null);
+			break;
+		case VERSION:
+			System.out.println("jSuneido " + WhenBuilt.when());
+			break;
+		}
+	}
 }
