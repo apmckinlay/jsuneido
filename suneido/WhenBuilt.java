5729be40eb749d07a1fd2e283dd34f264563b71e
amckinlay <amckinlay@localhost>
2010-03-30 23:30:58 +0000

    minor fix

--- suneido/WhenBuilt.java
+++ suneido/WhenBuilt.java
@@ -3,7 +3,7 @@
 			package suneido;
 			public class WhenBuilt {
 				public static String when() {
-					return "Mar 30 2010 11:14:47 (Java)";
+					return "Mar 30 2010 17:29:46 (Java)";
 				}
 			}
 		
