21cf66e285ddc2bc92a6468b5e04213ecf0fd937
Andrew McKinlay <apmckinlay@gmail.com>
2010-06-03 23:03:50 +0000

    fixed null pointer exception from invalid directory

--- suneido/WhenBuilt.java
+++ suneido/WhenBuilt.java
@@ -3,7 +3,7 @@
 			package suneido;
 			public class WhenBuilt {
 				public static String when() {
-					return "Jun 3 2010 13:42:56 (Java)";
+					return "Jun 3 2010 16:11:32 (Java)";
 				}
 			}
 		
