2069a2733558d480344575d306568c56e2a4bffe
Andrew McKinlay <apmckinlay@gmail.com>
2010-06-14 21:51:07 +0000

    fixed to remove leading slash before drive letter

--- src/suneido/language/builtin/ExePath.java
+++ src/suneido/language/builtin/ExePath.java
@@ -11,7 +11,10 @@ public class ExePath extends BuiltinFunction {
 	public Object call(Object... args) {
 		Args.massage(FunctionSpec.noParams, args);
 		URL uri = Suneido.class.getProtectionDomain().getCodeSource().getLocation();
-		return uri.getPath();
+		String path = uri.getPath();
+		if (path.matches("/[a-zA-Z]:.*"))
+			path = path.substring(1);
+		return path;
 	}
 
 	public static void main(String[] args) {
