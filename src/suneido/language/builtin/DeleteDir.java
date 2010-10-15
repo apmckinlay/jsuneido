29b00c8c26bba61843f09acf5a9ad2ecf1f78299
Andrew McKinlay <mckinlay@suneido.com>
2010-10-15 13:01:53 -0600

    fixed file deletion problems with Windows

--- src/suneido/language/builtin/DeleteDir.java
+++ src/suneido/language/builtin/DeleteDir.java
@@ -6,8 +6,6 @@ import java.io.IOException;
 import suneido.SuException;
 import suneido.language.*;
 
-import com.google.common.io.Files;
-
 public class DeleteDir extends BuiltinFunction {
 
 	@Override
@@ -18,11 +16,29 @@ public class DeleteDir extends BuiltinFunction {
 		if (!dir.isDirectory())
 			return false;
 		try {
-			Files.deleteRecursively(dir);
+			deleteRecursively(dir);
 		} catch (IOException e) {
 			throw new SuException("DeleteDir failed", e);
 		}
 		return true;
+	}
+
+	public static void deleteRecursively(File file) throws IOException {
+		if (file.isDirectory())
+			deleteDirectoryContents(file);
+		if (!file.delete())
+			throw new IOException("Failed to delete " + file);
+	}
+
+	// can't use Guava version because it can fail sometimes
+	// when !directory.getCanonicalPath().equals(directory.getAbsolutePath()
+	public static void deleteDirectoryContents(File directory)
+			throws IOException {
+		File[] files = directory.listFiles();
+		if (files == null)
+			throw new IOException("Error listing files for " + directory);
+		for (File file : files)
+			deleteRecursively(file);
 	}
 
 }
