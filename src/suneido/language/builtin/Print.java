b47247b0c499d1ae359a6e53bf974718a57950fb
Andrew McKinlay <apmckinlay@gmail.com>
2011-04-11 10:23:36 -0600

    changed to use ArgsIterator fixed to use display (now that toStr only does automatic conversions) added synchronized

--- src/suneido/language/builtin/Print.java
+++ src/suneido/language/builtin/Print.java
@@ -6,24 +6,33 @@ package suneido.language.builtin;
 
 import java.util.Map;
 
-import suneido.SuContainer;
 import suneido.language.*;
 
 public class Print extends SuFunction {
 
 	@Override
-	public Object call(Object... args) {
-		SuContainer c = Args.collectArgs(new SuContainer(), args);
-		StringBuilder sb = new StringBuilder();
-		int i = 0;
-		for (; i < c.vecSize(); ++i)
-			sb.append(i > 0 ? " " : "").append(Ops.toStr(c.get(i)));
-		for (Map.Entry<Object, Object> e : c.mapEntrySet())
-			sb.append(i++ > 0 ? " " : "").append(e.getKey()).append(": ")
-					.append(Ops.toStr(e.getValue()));
-		System.out.println(sb.toString());
+	public synchronized Object call(Object... args) {
+		ArgsIterator iter = new ArgsIterator(args);
+		while (iter.hasNext()) {
+			Object x = iter.next();
+			if (x instanceof Map.Entry) {
+				@SuppressWarnings("unchecked")
+                                Map.Entry<Object, Object> e = (Map.Entry<Object, Object>) x;
+				print(e.getKey());
+				print(": ");
+				print(e.getValue());
+			} else
+				print(x);
+			if (iter.hasNext())
+				print(" ");
+		}
+		System.out.println();
 		System.out.flush();
 		return null;
+	}
+
+	private void print(Object x) {
+		System.out.print(Ops.isString(x) ? Ops.toStr(x) : Ops.display(x));
 	}
 
 }
