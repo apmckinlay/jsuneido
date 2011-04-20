64f49cfa0769a3c2cde0974caddfbbb462cbf421
amckinlay <amckinlay@localhost>
2011-04-19 19:47:19 -0600

    immudb - working on database open

--- src/suneido/database/immudb/Database.java
+++ src/suneido/database/immudb/Database.java
@@ -6,6 +6,9 @@ package suneido.database.immudb;
 
 import java.nio.ByteBuffer;
 
+import suneido.database.immudb.Bootstrap.TN;
+import suneido.database.immudb.schema.Index;
+
 public class Database {
 	static final int INT_SIZE = 4;
 	public final Storage stor;
@@ -22,9 +25,60 @@ public class Database {
 		int root = buf.getInt();
 		int redirs = buf.getInt();
 System.out.println("open root " + root + " redirs " + redirs);
+		loadSchema(root, redirs);
+	}
 
+	private void loadSchema(int root, int redirs) {
+		Tran tran = new Tran(stor, redirs);
 		Record r = new Record(stor.buffer(root));
 System.out.println(r);
+		BtreeInfo info = Index.btreeInfo(r);
+		Btree indexesIndex = new Btree(tran, info);
+
+		int adr = indexesIndex.get(key(TN.COLUMNS, "table,column"));
+		r = new Record(stor.buffer(adr));
+System.out.println(r);
+		info = Index.btreeInfo(r);
+		Btree columnsIndex = new Btree(tran, info);
+
+		adr = indexesIndex.get(key(TN.TABLES, "table"));
+		r = new Record(stor.buffer(adr));
+System.out.println(r);
+		info = Index.btreeInfo(r);
+		Btree tablesIndex = new Btree(tran, info);
+
+		Btree.Iter iter;
+
+		iter = columnsIndex.iterator();
+		for (iter.next(); ! iter.eof(); iter.next()) {
+			Record key = iter.cur();
+			adr = Btree.getAddress(key);
+			r = new Record(stor.buffer(adr));
+System.out.println("column " + r);
+		}
+
+		iter = indexesIndex.iterator();
+		for (iter.next(); ! iter.eof(); iter.next()) {
+			Record key = iter.cur();
+			adr = Btree.getAddress(key);
+			r = new Record(stor.buffer(adr));
+System.out.println("index " + r);
+		}
+
+		iter = tablesIndex.iterator();
+		for (iter.next(); ! iter.eof(); iter.next()) {
+			Record key = iter.cur();
+			adr = Btree.getAddress(key);
+			r = new Record(stor.buffer(adr));
+System.out.println("table " + r);
+		}
+	}
+
+	private Record key(Object... values) {
+		RecordBuilder rb = new RecordBuilder();
+		for (Object x : values)
+			rb.add(x);
+		return rb.build();
 	}
 
 }
