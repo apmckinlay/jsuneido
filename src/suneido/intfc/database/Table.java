7465e9096ab7fa759bcb066c8f08be531d76e8cb
Andrew McKinlay <apmckinlay@gmail.com>
2011-07-14 17:12:04 -0600

    cleaning up dependencies, reducing coupling
    create suneido.intfc package

---
+++ src/suneido/intfc/database/Table.java
@@ -0,0 +1,23 @@
+/* Copyright 2011 (c) Suneido Software Corp. All rights reserved.
+ * Licensed under GPLv2.
+ */
+
+package suneido.intfc.database;
+
+import java.util.List;
+
+import suneido.database.Index;
+
+import com.google.common.collect.ImmutableList;
+
+public interface Table {
+	int num();
+	Index firstIndex();
+	boolean singleton();
+	List<String> getColumns();
+	List<List<String>> indexesColumns();
+	List<List<String>> keysColumns();
+	/** @return The physical fields. 1:1 match with records */
+	ImmutableList<String> getFields();
+	String schema();
+}
