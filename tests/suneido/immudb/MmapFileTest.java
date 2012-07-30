9dd411b915b373ee234485df247cfd61a4fac51f
Andrew McKinlay <apmckinlay@gmail.com>
2012-07-30 15:28:29 -0600

    tweaking tests

--- tests/suneido/immudb/MmapFileTest.java
+++ tests/suneido/immudb/MmapFileTest.java
@@ -9,20 +9,21 @@ import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.nio.ByteBuffer;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
+
+import suneido.util.FileUtils;
 
 public class MmapFileTest {
 
 	@Test(expected = RuntimeException.class)
 	public void cant_open() {
-		new MmapFile("tmp1", "r");
+		new MmapFile("must_not_exist", "r");
 	}
 
 	@Test
 	public void main() {
-		MmapFile mmf = new MmapFile("tmp2", "rw");
+		File tmp = FileUtils.tempfile();
+		MmapFile mmf = new MmapFile(tmp, "rw");
 		final int N = 128;
 		int adr = mmf.alloc(N);
 		ByteBuffer buf = mmf.buffer(adr);
@@ -30,18 +31,11 @@ public class MmapFileTest {
 			buf.put(i, (byte) i);
 		mmf.close();
 
-		mmf = new MmapFile("tmp2", "r");
+		mmf = new MmapFile(tmp, "r");
 		buf = mmf.rbuffer(-N);
 		for (int i = 0; i < N; ++i)
 			assertEquals(i, buf.get(i));
 		mmf.close();
-	}
-
-	@BeforeClass
-	@AfterClass
-	public static void cleanup() {
-		for (int i = 1; i <= 2; ++i)
-			new File("tmp" + i).delete();
 	}
 
 }
