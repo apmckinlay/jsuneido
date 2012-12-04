c386ff3b0eef8a4084c0e614b8a0155db2f86f1c
Andrew McKinlay <apmckinlay@gmail.com>
2012-12-04 14:35:55 -0600

    working on an improved implementation of rules

--- tests/suneido/SuRulesTest.java
+++ tests/suneido/SuRulesTest.java
@@ -8,13 +8,14 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import suneido.language.SuMethod0;
+
 public class SuRulesTest {
-	private static SuValue rule1 = new SuContainer(); // just need non-null SuValue
-	private static SuValue rule2 = new SuContainer(); // just need non-null SuValue
 
 	@Test
 	public void test_put_get() {
-		SuRules r = new NoRules();
+		SuRules r = new SuRules();
+		r.setDefault(null);
 		assertEquals("", r.get("nonexistent"));
 		r.put("a", 123);
 		assertEquals(123, r.get("a"));
@@ -22,117 +23,101 @@ public class SuRulesTest {
 		assertEquals(123, r.get("a"));
 		assertEquals(456, r.get("b"));
 	}
-	static class NoRules extends SuRules {
-		@Override
-		Object getRule(Object field) {
-			return null;
-		}
-	}
 
 	@Test
 	public void test_no_dependencies() {
-		SuRules r = new NoDeps();
+		SuRules r = new SuRules();
+		r.attachRule("a", new SuMethod0() {
+				@Override
+				public Object eval0(Object self) {
+					return "result";
+				}
+			});
 		assertEquals("result", r.get("a"));
 	}
-	static class NoDeps extends SuRules {
+
+	int count = 0;
+
+	SuMethod0 with_deps = new SuMethod0() {
 		@Override
-		Object getRule(Object field) {
-			return rule1;
+		public Object eval0(Object self) {
+			++count;
+			SuRules r = (SuRules) self;
+			r.get("a"); // get twice
+			return r.get("a").toString() + r.get("b").toString();
 		}
-		@Override
-		Object executeRule(Object rule) {
-			return "result";
-		}
-	}
+	};
 
 	@Test
 	public void test_dependencies() {
-		WithRule r = new WithRule();
+		SuRules r = new SuRules();
+		r.attachRule("r", with_deps);
 		r.put("a", 123);
 		r.put("b", 456);
 		assertEquals("123456", r.get("r"));
-		assertEquals(1, r.count);
+		assertEquals(1, count);
 		assertEquals("123456", r.get("r"));
-		assertEquals(1, r.count);
+		assertEquals(1, count);
 		r.put("b", "999");
-		assertEquals(1, r.count);
+		assertEquals(1, count);
 		assertEquals("123999", r.get("r"));
-		assertEquals(2, r.count);
+		assertEquals(2, count);
 		assertEquals("123999", r.get("r"));
-		assertEquals(2, r.count);
+		assertEquals(2, count);
 		// putting same value should be ignored
 		r.put("b", "999");
 		assertEquals("123999", r.get("r"));
-		assertEquals(2, r.count);
+		assertEquals(2, count);
 
 		r.forceInvalidate("r");
 		assertEquals("123999", r.get("r"));
-		assertEquals(3, r.count);
+		assertEquals(3, count);
 	}
-	static class WithRule extends NoDeps {
-		int count = 0;
 
-		@Override
-		Object getRule(Object field) {
-			return field.equals("r") ? rule1 : null;
-		}
-		@Override
-		Object executeRule(Object rule) {
-			++count;
-			get("a"); // get twice
-			return get("a").toString() + get("b").toString();
-		}
-	}
+	int count1;
+	int count2;
 
 	@Test
 	public void test_chained_rules() {
-		WithChainedRules r = new WithChainedRules();
+		SuRules r = new SuRules();
+		// r1 => r2 => a
+		r.attachRule("r1", new SuMethod0() {
+			@Override
+			public Object eval0(Object self) {
+				++count1;
+				return "=" + ((SuRules) self).get("r2").toString();
+			}});
+		r.attachRule("r2", new SuMethod0() {
+			@Override
+			public Object eval0(Object self) {
+				++count2;
+				return ((SuRules) self).get("a").toString().substring(0, 1); // first char
+			}});
 		r.put("a", "xy");
 		assertEquals("=x", r.get("r1"));
-		assertEquals(1, r.count1);
-		assertEquals(1, r.count2);
+		assertEquals(1, count1);
+		assertEquals(1, count2);
 		r.put("a", "XY");
 		assertEquals("=X", r.get("r1"));
-		assertEquals(2, r.count1);
-		assertEquals(2, r.count2);
+		assertEquals(2, count1);
+		assertEquals(2, count2);
 		r.put("a", "XZ");
 		assertEquals("=X", r.get("r1"));
-		assertEquals(3, r.count2);
-		assertEquals(2, r.count1);
+		assertEquals(3, count2);
+		assertEquals(2, count1);
 
 		assertEquals("[r2]", r.getdeps("r1").toString());
 		assertEquals("[a]", r.getdeps("r2").toString());
 		assertEquals("[]", r.getdeps("a").toString());
 	}
-	// r1 => r2 => a
-	static class WithChainedRules extends NoDeps {
-		int count1 = 0;
-		int count2 = 0;
-
-		@Override
-		Object getRule(Object field) {
-			return field.equals("r1") ? rule1
-					: field.equals("r2") ? rule2 : null;
-		}
-		@Override
-		Object executeRule(Object rule) {
-			if (rule == rule1) {
-				++count1;
-				return "=" + get("r2").toString();
-			} else if (rule == rule2) {
-				++count2;
-				return get("a").toString().substring(0, 1); // first char
-			} else
-				throw new RuntimeException("unreachable");
-		}
-	}
 
 	@Test
 	public void test_set_deps() {
-		WithRule r = new WithRule();
+		SuRules r = new SuRules();
+		r.attachRule("r", with_deps);
 		r.put("r", 123);
 		assertEquals(123, r.get("r"));
-		assertEquals(0, r.count);
+		assertEquals(0, count);
 		r.setdeps("r", "a,b");
 		assertEquals("[a, b]", r.getdeps("r").toString());
 		r.put("a", 12);
