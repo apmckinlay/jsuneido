a068135ecce8a9e98c1a1354d909e877e7607595
vcs <vcschapp@users.noreply.github.com>
2013-06-09 21:52:34 -0700

    Update JarPath.jarPath() to use URI.getPath() rather than URL.getPath(). The reason is that URL.getPath() uses URI escaping on URI reserved characters (i.e. ' ' -> '%20'). But the JVM exe doesn't expect URI escaping on the command line, so it couldn't find the JAR if the path contained spaces.

--- src/suneido/util/JarPath.java
+++ src/suneido/util/JarPath.java
@@ -1,12 +1,17 @@
 package suneido.util;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class JarPath {
 
 	public static String jarPath() {
-		URL uri = JarPath.class.getProtectionDomain().getCodeSource().getLocation();
-		String path = uri.getPath();
+		URL url = JarPath.class.getProtectionDomain().getCodeSource().getLocation();
+		String path = null;
+		try // try to use URI.getPath() to get unescaped reserved chars (URL.getPath() escapes them).
+			{ path = url.toURI().getPath(); }
+		catch (URISyntaxException e)
+			{ path = url.getPath(); }
 		if (path.matches("/[a-zA-Z]:.*"))
 			path = path.substring(1);
 		return path;
