7de77ce61b6b69759905170f66f86e5032c8eef7
vcschapp <vcschapp@users.noreply.github.com>
2013-08-19 16:22:23 -0700

    Dummy stub for SymbolsInfo() built-in

---
+++ src/main/java/suneido/language/builtin/SymbolsInfo.java
@@ -0,0 +1,18 @@
+package suneido.language.builtin;
+
+/**
+ * Implements the {@code SymbolsInfo} built-in.
+ * 
+ * @author Victor Schappert
+ * @since 20130819
+ */
+public final class SymbolsInfo {
+
+	private static final String DUMMY_STRING = "Symbols: Count 0 (max 0), Size 0 (max 0)";
+
+	public static String SymbolsInfo() {
+		// TODO: Implement me
+		return DUMMY_STRING;
+	}
+
+}
