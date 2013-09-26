425d22eaf67eb81f614a8996a16d88bfbecfb3ca
Andrew McKinlay <apmckinlay@gmail.com>
2013-09-26 14:21:33 -0600

    added missing xor keyword

--- src/main/java/suneido/language/Token.java
+++ src/main/java/suneido/language/Token.java
@@ -128,6 +128,7 @@ public enum Token {
 			if (t.string != null && Character.isLetter(t.string.charAt(0)))
 				keywords.put(t.string, t);
 		keywords.put("destroy", DROP);
+		keywords.put("xor", ISNT);
 	}
 	public static Token lookup(String s) {
 		return keywords.get(s);
