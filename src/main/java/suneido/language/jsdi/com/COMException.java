d9478400ea0970872e55c044f83b1353616915fe
vcschapp <vcschapp@localhost>
2013-11-03 21:58:05 -0800

    Implement COM support

---
+++ src/main/java/suneido/language/jsdi/com/COMException.java
@@ -0,0 +1,39 @@
+/* Copyright 2013 (c) Suneido Software Corp. All rights reserved.
+ * Licensed under GPLv2.
+ */
+
+package suneido.language.jsdi.com;
+
+import suneido.language.jsdi.DllInterface;
+import suneido.language.jsdi.JSDIException;
+
+/**
+ * Exception thrown when there's a problem using a Suneido {@code COMobject}.
+ *
+ * @author Victor Schappert
+ * @since 20131023
+ */
+@DllInterface
+public final class COMException extends JSDIException {
+
+	//
+	// SERIALIZATION
+	//
+
+	/**
+	 * Required to silence Java compiler warning.
+	 */
+	private static final long serialVersionUID = 8480803631452870823L;
+
+	//
+	// CONSTRUCTORS
+	//
+
+	public COMException(String message) {
+		super(message);
+	}
+
+	public COMException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
