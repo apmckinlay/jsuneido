a85f1ca4ae67e2341f2eaf6adebaaf4793cbf2cf
Andrew McKinlay <mckinlay@suneido.com>
2014-07-03 12:15:24 -0600

    renamed

--- src/test/java/suneido/language/TestMethodHandle.java
+++ src/test/java/suneido/language/TestMethodHandle.java
@@ -10,17 +10,16 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 
-public class MethodHandleTest {
+public class TestMethodHandle {
 
 	static Object func(Object... args) {
 		return args[0];
 	}
 
-	public static void main(String[] args)
-			throws Throwable {
+	public static void main(String[] args) throws Throwable {
 		MethodHandles.Lookup lookup = MethodHandles.lookup();
 		MethodType mt = MethodType.methodType(Object.class, Object[].class);
-		MethodHandle mh = lookup.findStatic(MethodHandleTest.class, "func", mt);
+		MethodHandle mh = lookup.findStatic(TestMethodHandle.class, "func", mt);
 		assertTrue(mh.isVarargsCollector());
 		assertEquals("hello", mh.invoke("hello", "world"));
 	}
