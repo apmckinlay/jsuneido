fa6bf47b7f7af76d699dfab806948c4327172a4b
vic <vcschapp@users.noreply.github.com>
2014-08-13 09:06:14 -0700

    Test coverage: add test for ThunkManager

---
+++ src/test/java/suneido/jsdi/ThunkManagerTest.java
@@ -0,0 +1,32 @@
+package suneido.jsdi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import suneido.language.Compiler;
+
+/**
+ * Test for {@link ThunkManager}.
+ *
+ * @author Victor Schappert
+ * @since 20140813
+ */
+public class ThunkManagerTest {
+
+	private static Object eval(String src) {
+		return Compiler.eval(src);
+	}
+
+	@Test
+	public void testClearNonCallable() {
+		assertSame(Boolean.FALSE,
+				ThunkManager.ClearCallback.ClearCallback(Boolean.FALSE));
+		assertSame(Boolean.FALSE, eval("ClearCallback('not a callback!')"));
+	}
+
+	@Test
+	public void testClearNotBound() {
+		assertSame(Boolean.FALSE, eval("ClearCallback(function() {})"));
+	}
+}
