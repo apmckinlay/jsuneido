577bbb28ac4bb0301597d96f5e5877be3bcb8b3f
vic <vic@localhost>
2014-08-21 10:45:38 -0700

    docs

--- src/main/java/suneido/jsdi/marshall/ObjectConversions.java
+++ src/main/java/suneido/jsdi/marshall/ObjectConversions.java
@@ -10,7 +10,10 @@ import suneido.jsdi.JSDIException;
 import suneido.language.Ops;
 
 /**
- * TODO: docs
+ * Utility methods to help marshalling code convert Java {@code Object}
+ * references to Suneido {@code Object()} (<i>ie</i> Java {@link SuContainer})
+ * references.
+ *
  * @author Victor Schappert
  * @since 20130718
  */
@@ -22,7 +25,41 @@ public final class ObjectConversions {
 				+ " to object");
 	}
 
-	// TODO: docs -- since 20130718
+	/**
+	 * <p>
+	 * Converts a Java {@link Object} reference, including a {@code null}
+	 * reference, to an {@link SuContainer} reference using the algorithm
+	 * described below.
+	 * </p>
+	 *
+	 * <p>
+	 * <ol>
+	 * <li>
+	 * if {@code value} is a {@code null} reference, returns a reference to a
+	 * newly instantiated {@link SuContainer} whose vector capacity is
+	 * {@code minCapacity};</li>
+	 * <li>
+	 * if {@code value} is convertible to an {@link SuContainer} reference via
+	 * via {@link Ops#toContainer(Object) Ops.toContainer(value)}, returns the
+	 * resulting container reference;</li>
+	 * <li>
+	 * otherwise throws a can't convert exception.</li>
+	 * </ol>
+	 * </p>
+	 *
+	 * @param value
+	 *            Value to convert (or {@code null}
+	 * @param vecCapacity
+	 *            Vector capacity to use when creating a new container (
+	 *            <em>only applies if {@code value} is {@code null})
+	 * @return Container reference that may, or may not, be reference-equal to
+	 *         {@code value}
+	 * @since 20130718
+	 * @throws JSDIException
+	 *             If {@code value} is neither {@code null} nor a value that can
+	 *             convert to {@link SuContainer}
+	 * @see #containerOrThrow(Object)
+	 */
 	public static SuContainer containerOrThrow(Object value, int vecCapacity) {
 		if (null == value) {
 			return new SuContainer(vecCapacity);
@@ -31,7 +68,21 @@ public final class ObjectConversions {
 		}
 	}
 
-	// TODO: docs -- since 20130808
+	/**
+	 * <p>
+	 * Converts a Java {@link Object} reference that is expected to be non-
+	 * {@code null} to an SuContainer reference via
+	 * {@link Ops#toContainer(Object)}.
+	 * </p>
+	 *
+	 * @param value
+	 *            Reference to convert to a container reference
+	 * @return Container reference that is reference-equal to {@code value}
+	 * @throws JSDIException
+	 *             If {@code value} is {@code null} or
+	 *             {@link Ops#toContainer(Object) Ops.toContainer(value)}
+	 *             returns {@code null}
+	 */
 	public static SuContainer containerOrThrow(Object value) {
 		assert null != value;
 		final SuContainer c = Ops.toContainer(value);
