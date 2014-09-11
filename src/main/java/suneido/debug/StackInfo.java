f7e542b6a60cbc670610ec1af2636984dc87b86b
vcschapp <vcschapp@localhost>
2014-09-11 14:11:27 -0700

    Temp commit for posterity (will roll back)

--- src/main/java/suneido/debug/StackInfo.java
+++ src/main/java/suneido/debug/StackInfo.java
@@ -64,4 +64,27 @@ final class StackInfo {
 	private void fetchInfo() {
 		// Placeholder for breakpoint
 	}
+
+	@SuppressWarnings("unused")
+	private void alloc(int numFrames) {
+		// Called from jsdebug agent or JDWP client. The reason for using a
+		// method to do this rather than just having the agent/client set the
+		// fields directly is that when using JDI to ask the JDWP agent to
+		// allocate a new array, there's no clean way to prevent the new array
+		// from being garbage collected before you can assign it to a field of
+		// the StackInfo instance to make it reachable. If instead we tell JDWP
+		// to call a method on a reachable StackInfo instance, we can make sure
+		// the new arrays are always reachable.
+		this.localsNames = new String[numFrames][];
+		this.localsValues = new Object[numFrames][];
+		this.isCall = new boolean[numFrames];
+		this.lineNumbers = new int[numFrames];
+	}
+
+	@SuppressWarnings("unused")
+	private void allocFrame(int frameIndex, int numLocals) {
+		// See note in alloc(int) above.
+		this.localsNames[frameIndex] = new String[numLocals];
+		this.localsValues[frameIndex] = new Object[numLocals];
+	}
 }
