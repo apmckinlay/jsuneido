19c8ac3f88b2ff4d5297766a3b8178a681f9b1b1
vcschapp <vcschapp@localhost>
2014-09-14 21:29:40 -0700

    - Commit current progress on "stack" mode debugging - I am going to strip out the whole concept of "stack" debugging in the next commit because it seems to me like a waste of time that doesn't add much to either of the other two debug modes

--- src/main/java/suneido/debug/CallstackStack.java
+++ src/main/java/suneido/debug/CallstackStack.java
@@ -6,9 +6,18 @@ package suneido.debug;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import suneido.SuValue;
+import suneido.TheDbms;
 import suneido.compiler.AstCompile;
 import suneido.compiler.ClassGen;
+import suneido.database.server.Dbms.LibGet;
+import suneido.jsdi.type.Structure;
+import suneido.runtime.BuiltinMethods;
+import suneido.runtime.CallableType;
+import suneido.runtime.Pack;
 
 /**
  * <p>
@@ -56,10 +65,8 @@ public final class CallstackStack extends Callstack {
 		final List<Frame> frames;
 		MethodName methodNameAbove = MethodName.UNKNOWN;
 		MethodName methodNameCur = MethodName.UNKNOWN;
-		String callableTypeAbove = null; // "callableType" means function,
-											// method, etc.
-		String callableTypeCur = null; // (there should really be an enum in
-										// runtime for this)
+		CallableType callableTypeAbove = CallableType.UNKNOWN;
+		CallableType callableTypeCur = CallableType.UNKNOWN;
 		String valueNameAbove = null;
 		String valueNameCur = null;
 
@@ -92,8 +99,31 @@ public final class CallstackStack extends Callstack {
 			frames.add(frame);
 		}
 
-		private String determineTypeOfFunction(String fullyQualifiedClassName) {
-			throw new Error("not implemented yet");
+		private boolean hasCompilerNameSuffix(String fullyQualifiedClassName) {
+			for (CallableType callableType : CallableType.values()) {
+				if (fullyQualifiedClassName.endsWith(callableType
+						.compilerNameSuffix())) {
+					callableTypeCur = callableType;
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private static final Pattern libraryPattern = Pattern
+				.compile("library\\[(^\\]]+)\\]->(.*)");
+
+		private static String[] extractLibraryFromFromFile(String sourceFileName) {
+			// NOTE: For this to be really useful, it would be nice to be able
+			// to include a version tag in the compiled code "file name"
+			// so we can look up the correct version of the source code.
+			Matcher m = libraryPattern.matcher(sourceFileName);
+			String[] result = new String[2];
+			if (m.matches()) {
+				result[0] = m.group(1);
+				result[1] = m.group(2);
+			}
+			return result;
 		}
 
 		private void filter(StackTraceElement element) {
@@ -124,48 +154,121 @@ public final class CallstackStack extends Callstack {
 			final int methodSep = fullyQualifiedClassName.indexOf(
 					AstCompile.METHOD_SEPARATOR, lastDot + 1);
 			if (0 <= methodSep) {
-				callableTypeCur = "method";
+				callableTypeCur = CallableType.METHOD;
 				valueNameCur = fullyQualifiedClassName.substring(lastDot + 1,
 						methodSep)
 						+ '.'
 						+ fullyQualifiedClassName.substring(methodSep + 1);
-			} else if (fullyQualifiedClassName.endsWith("$f")) {
-				// Function or block. At the moment, the only way to distinguish
-				// between the two is the following:
-				// 1) If the next frame below is a closure, it's a block.
-				// 2) Otherwise, load the class and determine if the
-				// immediate superclass is SuFunctionN or just SuCallable.
-				callableTypeCur = determineTypeOfFunction(fullyQualifiedClassName);
+			} else if (hasCompilerNameSuffix(fullyQualifiedClassName)) {
 				valueNameCur = methodNameCur.isCall() ? "call" : "eval";
 			} else {
 				// Library function
-				callableTypeCur = "function";
+				callableTypeCur = CallableType.FUNCTION;
 				valueNameCur = fullyQualifiedClassName.substring(lastDot + 1);
 			}
 			if (isNewSuneidoFrame()) {
-				addFrame(new FrameStack(valueNameCur, callableTypeCur, element.getFileName(), element.getLineNumber()));
+				String[] libraryAndItem = extractLibraryFromFromFile(element
+						.getFileName());
+				addFrame(new FrameStack(valueNameCur, callableTypeCur,
+						libraryAndItem[0], libraryAndItem[1],
+						element.getLineNumber()));
 			}
 		}
 	}
 
 	private static final class FrameStack extends Frame {
 
-		final int lineNumber;
+		private final String valueName;
+		private final CallableType callableType;
+		private final String library;
+		private final String libraryItemName;
+		private final int lineNumber;
 
-		public FrameStack(String valueName, String callableType,
-				String library, int lineNumber) {
+		public FrameStack(String valueName, CallableType callableType,
+				String library, String libraryItemName, int lineNumber) {
 			super(CallstackNone.EMPTY_LOCAL_ARRAY);
+			this.valueName = valueName;
+			this.callableType = callableType;
+			this.library = library;
+			this.libraryItemName = libraryItemName;
 			this.lineNumber = lineNumber;
 		}
 
+		//
+		// ANCESTOR CLASS: Frame
+		//
+
 		@Override
 		public Object getFrame() {
-			return "abc";
+			return new PseudoFunction(this);
 		}
 
 		@Override
 		public int getLineNumber() {
 			return lineNumber;
+		}
+
+		//
+		// INTERNAL TYPES
+		//
+
+		private static final class PseudoFunction extends SuValue {
+
+			//
+			// DATA
+			//
+
+			private final FrameStack frame;
+
+			//
+			// CONSTRUCTORS
+			//
+
+			private PseudoFunction(FrameStack frame) {
+				this.frame = frame;
+			}
+
+			//
+			// INTERNALS
+			//
+
+			private String sourceCode() {
+				if (null != frame.library && null != frame.libraryItemName) {
+					List<LibGet> candidates = TheDbms.dbms().libget(
+							frame.libraryItemName);
+					for (LibGet lg : candidates) {
+						if (lg.library.equals(frame.library)) {
+							return (String) Pack.unpack(lg.text);
+						}
+					}
+				}
+				return "// SOURCE NOT AVAILABLE"; // TODO improve this
+			}
+
+			//
+			// ANCESTOR CLASS: SuValue
+			//
+
+			@Override
+			public SuValue lookup(String method) {
+				return builtins.lookup(method);
+			}
+
+			@Override
+			public String display() {
+
+			}
+
+			//
+			// BUILT-IN METHODS
+			//
+
+			private static final BuiltinMethods builtins = new BuiltinMethods(
+					PseudoFunction.class);
+
+			public static Object Source(Object self) {
+				return ((PseudoFunction) self).sourceCode();
+			}
 		}
 	}
 }
