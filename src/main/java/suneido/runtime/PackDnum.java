e7189cfc95addb3d1c8b548108eac255ef8ef6ad
Andrew McKinlay <apmckinlay@gmail.com>
2014-09-14 20:12:34 -0600

    fixing bugs in Dnum packing (had to change format)

--- src/main/java/suneido/runtime/PackDnum.java
+++ src/main/java/suneido/runtime/PackDnum.java
@@ -15,11 +15,6 @@ import suneido.util.Dnum;
  * <li>packed format of zero is just Tag.PLUS
  * <li>exponent encoded to compare correctly as unsigned byte
  * <li>bytes of coefficient, most significant first
- * <li>number of bytes - 1 is in highest (first) 3 bits<br />
- * e.g. coefficient of 15 (0xf) would be encoded as 0xf (#bytes - 1 = 0)<br />
- * 		255 (0xff) would be encoded as 0x40ff
- * <li>trailing zero (decimal) digits are stripped
- * to reduce size and make comparisons work properly
  */
 public class PackDnum {
 
@@ -44,36 +39,54 @@ public class PackDnum {
 		buf.put(sign == -1 ? Tag.MINUS : Tag.PLUS);
 		if (n == 0)
 			return;
-		// strip trailing zeroes
+		if (e == Dnum.expInf) {
+			buf.put(expEncode(e, sign));
+			return;
+		}
+
+		// strip trailing zeroes to make coefficient smaller
 		while (Long.remainderUnsigned(n, 10) == 0) {
 			n = Long.divideUnsigned(n, 10);
 			e++;
 		}
+		// adjust exponent as if decimal was at start of digits (i.e. 0 <= coef < 1)
+		e += ndigits(n);
+		assert e <= Byte.MAX_VALUE : e;
+
 		// exponent
 		buf.put(expEncode(e, sign));
+
 		// coefficient
 		int nb = nbytes(n) - 1;
-		int shift = 8 * nb;
-		buf.put((byte) ((nb << 5) | (n >> shift)));
-		for (shift -= 8; shift >= 0; shift -= 8) {
-			buf.put((byte) (n >> shift));
+		for (int shift = 8 * nb; shift >= 0; shift -= 8) {
+			int b = (int) (n >> shift);
+			if (sign == -1)
+				b ^= 0xff;
+			buf.put((byte) b);
 		}
+	}
+
+	/** @return the number of decimal digits in x */
+	private static int ndigits(long x) {
+		int n = 0;
+		while (x != 0) {
+			x = Long.divideUnsigned(x, 10);
+			n++;
+		}
+		return n;
 	}
 
 	private static byte expEncode(int e, int sign) {
 		byte eb = (byte) ((e ^ 0x80) & 0xff);
 		if (sign == -1)
-			eb = (byte) ((~eb) & 0xff);
+			eb = (byte) ((~eb) & 0xff); // reverse sort order for negative
 		return eb;
 	}
 
-	/**
-	 * @return The number of bytes required to hold n
-	 * allowing 3 bits for this count
-	 */
+	/** @return The number of bytes required to hold n */
 	static int nbytes(long x) {
-		int n = 1;
-		for (; (x & ~0x1f) != 0; ++n)
+		int n = 0;
+		for (; x != 0; ++n)
 			x >>>= 8;
 		return n;
 	}
@@ -100,10 +113,16 @@ public class PackDnum {
 			e = ((~e) & 0xff);
 		e = (byte) (e ^ 0x80);
 		// coefficient
-		long n = buf.get() & 0x1f; // strip nbytes (only needed for sorting)
-		while (buf.remaining() > 0)
-			n = (n << 8) | (buf.get() & 0xff);
+		long n = 0;
+		while (buf.remaining() > 0) {
+			int b = buf.get() & 0xff;
+			if (sign == -1)
+				b ^= 0xff;
+			n = (n << 8) | b;
+		}
 		assert n != 0;
+		e -= ndigits(n); // restore exponent to right of digits
+
 		// return as Integer if within range
 		if (0 <= e && e < 10 && Long.compareUnsigned(n, MAX_INT_DIV_10) < 0) {
 			for (; e > 0 && n < MAX_INT_DIV_10; --e)
