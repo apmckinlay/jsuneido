ed4715b303ffb8933bed9ff1be63dba782d55091
Andrew McKinlay <apmckinlay@gmail.com>
2015-02-05 09:23:35 -0600

    default ServerPort to 3147 to match cSuneido

--- src/main/java/suneido/TheDbms.java
+++ src/main/java/suneido/TheDbms.java
@@ -18,7 +18,7 @@ import suneido.util.Print;
 public class TheDbms {
 	private static final long IDLE_TIMEOUT_MS = 5 * 60 * 1000; // 5 min
 	private static String ip = null;
-	private static int port;
+	private static int port = 3147;
 	private static DbmsLocal localDbms;
 	private static final ThreadLocal<DbmsRemote> remoteDbms =
 			new ThreadLocal<>();
