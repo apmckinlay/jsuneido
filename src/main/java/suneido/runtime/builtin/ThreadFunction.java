c9c61e4f1c73cd073937e3f3e44f20675f3156f5
Andrew McKinlay <apmckinlay@gmail.com>
2015-03-31 10:34:10 -0600

    propagate authorization to child threads

--- src/main/java/suneido/runtime/builtin/ThreadFunction.java
+++ src/main/java/suneido/runtime/builtin/ThreadFunction.java
@@ -4,9 +4,11 @@
 
 package suneido.runtime.builtin;
 
+import suneido.TheDbms;
 import suneido.runtime.Ops;
 import suneido.runtime.Params;
 import suneido.util.Errlog;
+import suneido.util.Util;
 
 public class ThreadFunction {
 
@@ -20,14 +22,21 @@ public class ThreadFunction {
 
 	private static class Callable implements Runnable {
 		private final Object callable;
+		private final byte[] token;
 
 		public Callable(Object callable) {
+			// runs in the parent thread
 			this.callable = callable;
+			token = TheDbms.dbms().token();
+			// token may be empty if parent not authorized
 		}
 
 		@Override
 		public void run() {
+			// runs in the child thread
 			try {
+				// auth will only succeed if parent was authorized
+				TheDbms.dbms().auth(Util.bytesToString(token));
 				Ops.call(callable);
 			} catch (Throwable e ) {
 				Errlog.uncaught("in thread", e);
