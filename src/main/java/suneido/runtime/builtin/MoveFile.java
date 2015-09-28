ae015abf37c54d30fb913680c75428da168f320c
Andrew McKinlay <apmckinlay@gmail.com>
2015-09-28 11:33:38 -0600

    changed MoveFile to overwrite

--- src/main/java/suneido/runtime/builtin/MoveFile.java
+++ src/main/java/suneido/runtime/builtin/MoveFile.java
@@ -7,6 +7,7 @@ package suneido.runtime.builtin;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 import suneido.runtime.Ops;
 import suneido.runtime.Params;
@@ -16,7 +17,8 @@ public class MoveFile {
 	@Params("from, to")
 	public static Boolean MoveFile(Object from, Object to) {
 		try {
-			Files.move(Paths.get(Ops.toStr(from)), Paths.get(Ops.toStr(to)));
+			Files.move(Paths.get(Ops.toStr(from)), Paths.get(Ops.toStr(to)),
+					StandardCopyOption.REPLACE_EXISTING);
 			return true;
 		} catch (IOException e) {
 			return false;
