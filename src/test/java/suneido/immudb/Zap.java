9639b45fe243ddd0e0378e1fea0c391e0771ef5a
Andrew McKinlay <apmckinlay@gmail.com>
2015-12-17 09:21:33 -0600

    comments

--- src/test/java/suneido/immudb/Zap.java
+++ src/test/java/suneido/immudb/Zap.java
@@ -7,15 +7,18 @@ package suneido.immudb;
 import java.nio.ByteBuffer;
 
 /**
- * corrupt the end of the file
- * so it appears the database was not shut down properly
+ * useful to corrupt the database to test check & rebuild
  */
 public class Zap {
 
 	public static void main(String[] args) {
 		String filename = "suneido.dbi";
 		try (MmapFile mmf = new MmapFile(filename, "rw")) {
-			long offset = -56; // negative for end relative
+			// negative for end relative
+			//long offset = 56; // will make dbi unusable 
+			//long offset = -56; // quick rebuild, detected by fast check
+			//long offset = -500000; // not detected by fast check, quick rebuild
+			long offset = 500000; // not detected by fast check, slower rebuild
 			ByteBuffer buf = offset < 0
 					? mmf.rbuffer(offset)
 					: mmf.buffer(Storage.offsetToAdr(offset));
