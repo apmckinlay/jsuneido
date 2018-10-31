551c2034746d21cc294335eb8db262b96494ec80
Andrew McKinlay <apmckinlay@gmail.com>
2018-10-31 13:58:30 -0600

    change Dir to be a Sequence

--- src/main/java/suneido/runtime/builtin/Dir.java
+++ src/main/java/suneido/runtime/builtin/Dir.java
@@ -7,34 +7,105 @@ package suneido.runtime.builtin;
 import static suneido.runtime.Ops.toStr;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.util.regex.Pattern;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Iterator;
+
+import com.google.common.collect.Iterators;
 
 import suneido.SuContainer;
 import suneido.SuDate;
 import suneido.runtime.Ops;
 import suneido.runtime.Params;
+import suneido.runtime.Sequence;
 import suneido.util.Dnum;
 
 public class Dir {
-
-	@Params("path = '*.*', files = false, details = false")
-	public static SuContainer Dir(Object p, Object f, Object d) {
+	@Params("path = '*', files = false, details = false")
+	public static Object Dir(Object p, Object f, Object d) {
 		boolean files = Ops.toBoolean_(f);
 		boolean details = Ops.toBoolean_(d);
-		File path = new File(toStr(p));
-		String dir = path.getParent();
-		if (dir == null)
+		String dir = toStr(p);
+		String glob;
+		int i = dir.lastIndexOf('/');
+		if (i == -1) {
+			glob = dir;
 			dir = ".";
-		String pattern = path.getName();
-		FileFilter filter = new Filter(pattern);
-		SuContainer ob = new SuContainer();
-		File[] listFiles = new File(dir).listFiles(filter);
-		if (listFiles != null)
-			for (File file : listFiles)
-				if (! files || ! file.isDirectory())
-					ob.add(details ? detailsOf(file) : nameOf(file));
-		return ob;
+		} else {
+			glob = dir.substring(i + 1);
+			dir = dir.substring(0, i);
+		}
+		if (glob.endsWith("*.*")) // *.* only matches if there is a literal '.'
+			glob = glob.substring(0, glob.length() - 2);
+		Path path = FileSystems.getDefault().getPath(dir); // Path.of for 11
+		return new Sequence(new SuDir(path, glob, files, details));
+	}
+
+	private static class SuDir implements Iterable<Object> {
+		final Path dir;
+		final String glob;
+		final boolean files;
+		final boolean details;
+
+		SuDir(Path dir, String glob, boolean files, boolean details) {
+			this.dir = dir;
+			this.glob = glob;
+			this.files = files;
+			this.details = details;
+		}
+
+		@Override
+		public Iterator<Object> iterator() {
+			DirectoryStream<Path> ds;
+			Iterator<Path> iter;
+			try {
+				ds = Files.newDirectoryStream(dir, glob);
+				iter = ds.iterator();
+			} catch (IOException e) {
+				return Collections.emptyIterator();
+			}
+			if (files)
+				iter = Iterators.filter(iter,
+						(Path p) -> !p.toFile().isDirectory());
+			return new Iter(ds, iter);
+		}
+
+		class Iter implements Iterator<Object> {
+			final DirectoryStream<Path> ds;
+			final Iterator<Path> iter;
+
+			Iter(DirectoryStream<Path> ds, Iterator<Path> iter) {
+				this.ds = ds;
+				this.iter = iter;
+			}
+
+			@Override
+			public boolean hasNext() {
+				if (iter.hasNext())
+					return true;
+				try {
+					ds.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				return false;
+			}
+
+			@Override
+			public Object next() {
+				File file = iter.next().toFile();
+				return details ? detailsOf(file) : nameOf(file);
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+		}
 	}
 
 	private static String nameOf(File f) {
@@ -61,28 +132,6 @@ public class Dir {
 		return (f.canWrite() ? 0 : READONLY)
 				| (f.isHidden() ? HIDDEN : 0)
 				| (f.isDirectory() ? DIRECTORY : 0);
-	}
-
-	private static class Filter implements FileFilter {
-		private final Pattern pattern;
-
-		Filter(String s) {
-			pattern = Pattern.compile(convert(s), Pattern.CASE_INSENSITIVE);
-		}
-
-		private static Pattern SPECIAL_REGEX_CHARS =
-				Pattern.compile("[{}()\\[\\].+^$\\\\|]");
-		private static String convert(String s) {
-			s = s.replace("*.*", "*"); // for compatibility with windows
-			s = SPECIAL_REGEX_CHARS.matcher(s).replaceAll("\\\\$0");
-			return s.replace("?", ".").replace("*", ".*");
-		}
-
-		@Override
-		public boolean accept(File f) {
-			return pattern.matcher(f.getName()).matches();
-		}
-
 	}
 
 }
