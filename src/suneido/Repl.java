ad0fd345e8ace01373b48ab1ecf48472304b9c4d
Andrew McKinlay <apmckinlay@gmail.com>
2019-11-05 20:09:27 -0600

    change repl to multiline input

--- src/suneido/Repl.java
+++ src/suneido/Repl.java
@@ -4,13 +4,20 @@
 
 package suneido;
 
-import java.io.*;
+import static java.lang.System.err;
+import static java.lang.System.out;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import suneido.compiler.Compiler;
 import suneido.database.immudb.Dbpkg;
 import suneido.runtime.Concats;
 import suneido.runtime.Except;
 import suneido.runtime.Ops;
+import suneido.util.Dnum;
 import suneido.util.Errlog;
 
 /*
@@ -34,7 +41,6 @@ public class Repl {
 	}
 
 	public static void repl2() throws IOException {
-		PrintWriter out = new PrintWriter(System.out);
 		try {
 			Compiler.eval("Init()");
 		} catch (SuException e) {
@@ -43,29 +49,21 @@ public class Repl {
 			else
 				throw e;
 		}
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		out.print("Built: " + Suneido.built);
-		out.println();
+		err.println("Built: " + Suneido.built);
+		err.println("Press Enter twice (i.e. blank line) to execute, q to quit");
 
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder code = new StringBuilder(1024);
 		repl: while (true) {
-			out.print("> ");
-			out.flush();
+			err.println("~~~");
 			code.delete(0,  code.length());
-			// Collect 1 or more lines of input: if a line ends with '\', it
-			// indicates to ignore the '\' and continue reading the next line.
 			while (true) {
 				String line = in.readLine();
 				if (line == null || "q".equals(line))
 					break repl;
 				if (line.isEmpty())
 					break;
-				if ('\\' == line.charAt(line.length() - 1)) {
-					code.append(line, 0, line.length() - 1).append('\n');
-				} else {
-					code.append(line);
-					break;
-				}
+				code.append(line);
 			}
 			// Evaluate the code
 			try {
@@ -74,22 +72,24 @@ public class Repl {
 					String type = Ops.typeName(result);
 					if (result instanceof Concats)
 						type = " <Concat>";
+					else if (result instanceof Integer)
+						type = " <Integer>";
+					else if (result instanceof Dnum)
+						type = " <Dnum>";
 					else if (result instanceof Except)
 						type = " <Except>";
-					else if (type.equals("String") || type.equals("Number") ||
+					else if (type.equals("String") ||
 							type.equals("Boolean") || type.equals("Date"))
 						type = "";
 					else
 						type = " <" + type + ">";
-					out.println(" => " + Ops.display(result) + type);
-
+					out.println(Ops.display(result) + type);
+					Thread.sleep(100); // needed for Eclipse to sync output
 				}
 			} catch (Throwable e) {
 				Errlog.error("Repl", e);
 			}
 		} // repl: while (true)
-		out.println("bye");
-		out.flush();
 	}
 
 }
