863988d1966b35f1cea0fb7774cd257424b7d558
Andrew McKinlay <apmckinlay@gmail.com>
2020-05-09 07:51:46 -0600

    workaround for Eclipse bug

--- src/suneido/database/server/DbmsLocal.java
+++ src/suneido/database/server/DbmsLocal.java
@@ -38,7 +38,13 @@ public class DbmsLocal extends Dbms {
 
 	@Override
 	public DbmsTran transaction(boolean readwrite) {
-		Transaction t = readwrite ? db.updateTransaction() : db.readTransaction();
+		// temporary work around for Eclipse bug, revert when Eclipse fixed
+		Transaction t;
+		if (readwrite)
+		    t = db.updateTransaction();
+		else
+		    t = db.readTransaction();
+		// Transaction t = readwrite ? db.updateTransaction() : db.readTransaction();
 		return new DbmsTranLocal(t);
 	}
 
