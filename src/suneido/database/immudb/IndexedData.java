fa4e5c42538b22d769794bdc5620d07cf5ab2f4d
Andrew McKinlay <apmckinlay@gmail.com>
2021-08-09 14:06:09 -0700

    cosmetic

--- src/suneido/database/immudb/IndexedData.java
+++ src/suneido/database/immudb/IndexedData.java
@@ -1,7 +1,3 @@
-/* Copyright 2012 (c) Suneido Software Corp. All rights reserved.
- * Licensed under GPLv2.
- */
-
 /* Copyright 2010 (c) Suneido Software Corp. All rights reserved.
  * Licensed under GPLv2.
  */
